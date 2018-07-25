5b1b04226ed1b1f457ff5604fd15a730b02ca4a1
dominiquem <dominique.meaux@gmail.com>
2018-07-25 16:14:53 +0200

    php part.exo2

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8"/>
+    <title>p1exo1</title>
+  </head>
+  <body>
+    <p>
+      <?php
+        $lastname = 'Meaux';
+        $firstname = 'Dominique';
+        $age = '34ans';
+        echo $lastname.' '.$firstname.' '.$age;
+      ?>
+    </p>
+  </body>
+</html>
